c81b1ecfaf4c7639be384e3bc2cdf99b7fbeebb7
gagahpangeran <gagahpangeran@gmail.com>
2018-05-09 19:01:57 +0700

    edit file to deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ReactGA from "react-ga";
-import App from "./routes";
-import "./styles/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+import App from './routes';
+import './styles/index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactGA.initialize("UA-112745741-2", { debug: false });
+ReactGA.initialize('UA-112745741-2', { debug: false });
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
